7e1ce465b7b4fca2b638bc3191d3be191ba9f82d
RyanAdamGonzalez1996 <ryangonzalezportfolio@gmail.com>
2020-12-19 13:58:51 -0500

    Finished up Psuedo-Code, will commence coding now.

--- UFO_Level_1/static/js/app.js
+++ UFO_Level_1/static/js/app.js
@@ -11,7 +11,31 @@ var form = d3.select("#form");
 button.on("click",);
 form.on("submit", );
 
+// Function to create and append data to the table
+function createTable(date, city, state, country, shape, duration, comments){
+    
+    // Select the table element by class name
+
+    // Initialize the table element to be blank
+
+    // Append the data to the table
+};
+
 // Event Handler Function
 function runEvent(){
 
+    // Prevent Page from Refreshing
+    d3.event.preventDefault();
+
+    // Select the input element and get the raw HTML node
+
+    // Get the input value (date) from the input element
+
+    // Filter the data.js by the input value
+    
+    // Set the information for the corresponding date into variables
+
+    // Call the createTable function with the corresponding 
+    // date with the parameters for each variable
 };
+
