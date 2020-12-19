a359e40c26fd5160b8565e994d0cc013296b8040
RyanAdamGonzalez1996 <ryangonzalezportfolio@gmail.com>
2020-12-19 13:38:27 -0500

    Set up initializing of function and variables

--- UFO_Level_1/static/js/app.js
+++ UFO_Level_1/static/js/app.js
@@ -1,4 +1,17 @@
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
+// Initialize the button instance
+var button = d3.select("#button");
+
+// Initialize the form instance
+var form = d3.select("#form");
+
+// Create Event handlers
+button.on("click",);
+form.on("submit", );
+
+// Event Handler Function
+function runEvent(){
+
+};
