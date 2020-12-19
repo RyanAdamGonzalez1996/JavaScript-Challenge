1d7828c3270f4329e98d0970f5eb7fd3ae47faa7
RyanAdamGonzalez1996 <ryangonzalezportfolio@gmail.com>
2020-12-19 17:04:49 -0500

    Attempting optional assignment, no luck getting the filters to check.

--- UFO_Level_2/static/js/app.js
+++ UFO_Level_2/static/js/app.js
@@ -11,6 +11,8 @@ var form = d3.select("#form");
 button.on("click", runEvent);
 form.on("submit", runEvent);
 
+//d3.selectAll(".filter").on("change", runFilter);
+
 // Function to create and append data to the table
 function createTable(filteredData){
     
@@ -40,23 +42,46 @@ function createTable(filteredData){
 
 };
 
+// Function to apply the filters that are selected
+function applyFilters(){
+
+};
+
+// Function to check which Filters are selectd
+function checkFilters(){
+    var usedFilters = [];
+    var ul = document.getElementById("filters");
+    var filters = ul.getElementsByTagName("li");
+    var test = filters[0].
+    console.log(filters);
+   // for (var i = 0; i < 7; i++){
+    //    var ul = d3.g
+     //   var li = li.select("input").property("value");
+     //   usedFilters.push(test);
+    //}
+    console.log(usedFilters);
+};
+
 // Event Handler Function
 function runEvent(){
 
     // Prevent Page from Refreshing
     d3.event.preventDefault();
 
+    checkFilters();
     // Select the input element and get the raw HTML node
-    var inputElement = d3.select("#datetime");
+    //var inputElement = checkFilters();
 
     // Get the input value (date) from the input element
-    var inputData = inputElement.property("value");
+    //var inputData = inputElement.property("value");
 
+    // Test
+    //console.log(inputData);
     // Filter the data.js by the input value
-    var filteredData = tableData.filter(sighting => sighting.datetime === inputData);
+    //var filteredData = tableData.filter(sighting => sighting.datetime === inputData);
 
     // Call the createTable function with the 
     // filteredData as the parameter
-    createTable(filteredData);
+    //createTable(filteredData);
 };
 
